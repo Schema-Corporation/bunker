03aaf0203ca2e0aece37ff63a11dccab54192d45
Marcelo Rios <u201521895@upc.edu.pe>
2019-09-06 00:13:57 -0500

    Adding jwt authentication to API server

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_045111) do
+ActiveRecord::Schema.define(version: 2019_09_06_050220) do
 
   create_table "booking_processes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "lessor_id"
@@ -118,6 +118,14 @@ ActiveRecord::Schema.define(version: 2019_09_06_045111) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["lessee_id"], name: "index_spaces_on_lessee_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "booking_processes", "documents"
