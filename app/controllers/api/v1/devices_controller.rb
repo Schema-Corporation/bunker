c450fc2c0ddbd1f0d7770d7b58e4f788da25eaa0
victor <u201413960@upc.edu.pe>
2019-09-07 14:06:43 -0500

    Handling exceptions for some API methods

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,43 +1,68 @@
 module Api 
     module V1 
-        class DeviceController < ApplicationController
+        class DevicesController < ApplicationController
 
 
             # GET /devices
             def index
-                @devices = Devices.all
+                @devices = Device.all
+                render json: {status: 'success', message:'Loaded devices', data:@devices},status: :ok
 
-                render json: @devices
             end
 
             # GET /devices/1
             def show
-                render json: @device
+                @device = Device.find(params[:id])
+                if @device != nil 
+                render json: {status: 'success', message:'Loaded device', data:@device,code: 200}
+                else 
+                render json: {status: 'FAILS', message:'Loaded device', data:nil,code: 404}
+            
+             end 
             end
 
             #POST /devices
             def create
-                @device = Devices.new(device_params)
-                if @device.save
-                    render json: @device, status: :created, location: @device
+                @devices = Device.new(device_params)
+                if @devices.save
+                    render json: {code:200, status: 'success', message:'Saved device', data:@devices},status: :ok
                 else
-                    render json: @device.errors, status: :unprocessable_entity
+                    render json: {code:404, status: 'ERROR', message:'Device not saved'}
+
                 end
             end
 
             #PATCH/PUT /devices/1
             def update
-                if @device.update(device_params)
-                    render json: @device
+                if @devices.update(device_params)
+                    #render json: @device
+                    render json: {status: 'success', message:'Updated Advice', data:@devices},status: :ok
                 else
-                    render json: @device.errors, status: :unprocessable_entity
+                    #render json: @device.errors, status: :unprocessable_entity
+                    render json: {status: 'ERROR', message:'Article not updated'}
+
                 end
             end
 
             #DELETE /devices/1
             def destroy
-                @device.destroy
+                @device = Device.find(params[:id])
+                if @device.destroy
+                    render json: {code:200,status: 'success', message:'Device deleted ', data:@devices}
+
+                end
+
+                rescue ActiveRecord::RecordNotFound
+                    render json: {code:404  ,status: 'Not Found', message: "Device not found", data:nil}
             end
+            
+            private 
+            # Only allow a trusted parameter "white list" through.
+            def device_params
+                # Reemplazamos :password_digest por :password y :password_confirmation
+                params.require(:device).permit(:device_token)
+            end
+
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
       # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
       get 'document_types' => 'document_types#index'
       resources :document_types
+
+      get 'devices' => 'devices#index'
+      get 'devices/:id' => 'devices#show'
+      post 'devices' => 'devices#create'
+      delete 'devices/:id' => 'devices#destroy'
+      resources :devices
     end
   end
 end
