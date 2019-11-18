1587cb4c1b9b9210c3cbc630aef1742626a9e5b5
Marcelo Rios <u201521895@upc.edu.pe>
2019-11-17 22:22:14 -0500

    fix for displaying data in endpoint request

--- app/serializers/booking_process_serializer.rb
+++ app/serializers/booking_process_serializer.rb
@@ -1,5 +1,5 @@
 class BookingProcessSerializer < ActiveModel::Serializer
-    attributes :id, :step, :start_date, :end_date, :monthly_fee, :created_at, :updated_at
+    attributes :id, :step, :start_date, :end_date, :monthly_fee, :created_at, :updated_at, :periodicity, :width, :height
     has_one :lessee
     has_one :space
     has_one :document
